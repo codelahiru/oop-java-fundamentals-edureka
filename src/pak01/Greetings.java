30664160203706e11a0806fa5b58e5289f5ab3aa
codelahiru <gayan.hatharasinghe@gmail.com>
2023-06-22 23:14:56 +0530

    062223-11.14

--- src/pak01/Greetings.java
+++ src/pak01/Greetings.java
@@ -2,15 +2,21 @@ package pak01;
 
 public class Greetings {
 	
-	public void test() {
+	public void nonStaticTest() {
 		System.out.println("Non static method");
 	}
+	
+	public void staticTest() {
+		System.out.println("Static method");
+	}
+
 
 	public static void main(String[] args) {
 		
 		Greetings greetingsObject = new Greetings();
 		//System.out.println("Hello world!!!");
-		greetingsObject.test();
+		greetingsObject.nonStaticTest();
+		greetingsObject.staticTest();
 
 	}
 
